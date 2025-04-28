a67d8a13fae158ee9c6a99db0a3bcfac00937c8b
Cheryl Lam <cwl92@cornell.edu>
2025-04-28 15:16:29 -0400

    Revise make_random_game_test

--- test/test_cs3110_final_project.ml
+++ test/test_cs3110_final_project.ml
@@ -13,24 +13,6 @@ let rec print_list lst =
   | (x, y) :: t ->
       "(" ^ string_of_int x ^ ", " ^ string_of_int y ^ ")" ^ "; " ^ print_list t
 
-let make_connection_test =
-  "connect (0,0) and (0,1)" >:: fun _ ->
-  let new_board = make_connection (0, 0) (0, 1) board_2x2 in
-  let lst = get_grid new_board in
-
-  assert_bool "(0,0) should be connected to (0,1)"
-    (List.exists
-       (fun ((x, y), neighbors) -> (x, y) = (0, 0) && List.mem (0, 1) neighbors)
-       lst);
-
-  assert_bool "(0,1) should be connected to (0,0)"
-    (List.exists
-       (fun ((x, y), neighbors) -> (x, y) = (0, 1) && List.mem (0, 0) neighbors)
-       lst)
-
-let make_connection_tests =
-  "test suite for make_connection" >::: [ make_connection_test ]
-
 (** [make_check_completed_box_test test_name (x1, y1) (x2, y2) spacing board
      expected_output] creates a test case named [test_name]. It tests whether
     adding a line connecting [(x1,y1)] and [(x2,y2)] forms a box in [grid] with
@@ -80,7 +62,7 @@ let board_4x4_two_box_horizontal =
   board_7
 
 let check_completed_box_tests =
-  "test suite for check_completed_box"
+  "Test suite for check_completed_box"
   >::: [
          make_check_completed_box_test "box is not completed" (0, 0) (0, 1) 0
            board_2x2 [];
@@ -106,54 +88,6 @@ let check_completed_box_tests =
            "two boxes completed after adding a horizontal line" (0, 1) (1, 1) 1
            board_4x4_two_box_horizontal
            [ (0, 0); (0, 1) ];
-       ]
-
-(** [make_completed_boxes_test test_name board expected_output] creates a test
-    case to check whether [completed_boxes grid] matches the actual number of
-    completed boxes in the game. *)
-let make_completed_boxes_test test_name board expected_output =
-  test_name >:: fun _ ->
-  (* print_endline (string_of_int (completed_boxes board)); *)
-  print_endline "\n Expected ";
-  print_string (string_of_int expected_output);
-  print_endline "Actual ";
-  print_string (string_of_int (completed_boxes board));
-  assert_equal expected_output (completed_boxes board)
-
-let board_2x2_one_box_comp =
-  let board_1 = make_connection (0, 0) (0, 1) board_2x2 in
-  let board_2 = make_connection (0, 1) (1, 1) board_1 in
-  let board_3 = make_connection (1, 1) (1, 0) board_2 in
-  let _ = get_box_coordinates (0, 0) (0, 1) 100 board_3 0 in
-  board_3
-
-let completed_boxes_tests =
-  "test suite for make_completed_boxes"
-  >::: [
-         make_completed_boxes_test "no completed boxes" (make_grid 2 0) 0;
-         (* make_completed_boxes_test "no completed boxes" (let _ =
-            completed_box_coordinates (0, 0) (0, 1) 100 board_2x2_one_box_comp
-            in board_2x2_one_box) 1; *)
-       ]
-
-(** [make_is_game_over test_name board size expected_output] creates a test case
-    to check whether [is_game_over grid size] correctly identifies if the game
-    is complete. *)
-let make_is_game_over_test test_name board size expected_output =
-  test_name >:: fun _ ->
-  (* print_endline (string_of_int (completed_boxes board)); *)
-  assert_equal expected_output (is_game_over board size)
-
-let is_game_over_tests =
-  "test suite for make_completed_boxes"
-  >::: [
-         make_is_game_over_test "a game with no completed boxes is not over"
-           (make_grid 2 0) 2 false;
-         (* make_is_game_over_test "a game with a 2x2 grid with 1 completed box
-            is over" (board_2x2_one_box) 2 true; *)
-         make_is_game_over_test
-           "a game with a 4x4 grid with 2 completed boxes is over"
-           board_4x4_two_box_vertical 4 false;
        ]
 
 (** [make_is_valid_move test_name (x1, y1) (x2, y2) spacing size board
@@ -245,7 +179,7 @@ let play_random_game grid spacing size num_players =
     simulating a game with [num_players] players on a [size x size] grid. *)
 let make_play_random_game_test test_name size num_players =
   test_name >:: fun _ ->
-  let spacing = 1 in
+  let spacing = 100 in
   let grid = make_grid size num_players in
 
   (* Initial board should have available moves. *)
@@ -270,31 +204,26 @@ let make_play_random_game_test test_name size num_players =
   let total_score =
     List.fold_left (fun acc (_, score) -> acc + score) 0 scores
   in
-  assert_equal ((size - 1) * (size - 1)) total_score ~printer:string_of_int
-(* There should be no more available moves. *)
-(* let final_points_with_moves =
+  assert_equal ((size - 1) * (size - 1)) total_score ~printer:string_of_int;
+  (* There should be no more available moves. *)
+  let final_points_with_moves =
     List.filter
       (fun p -> has_available_moves p spacing size final_board)
       all_points
   in
-  assert_equal 0 (List.length final_points_with_moves) ~printer:string_of_int *)
+  assert_equal 0 (List.length final_points_with_moves) ~printer:string_of_int
 
 let play_random_game_test =
-  "Test suite to simulate playing a game until completion"
+  "Simulate playing a game until completion"
   >::: [
          make_play_random_game_test "4x4 game" 4 2;
          make_play_random_game_test "6x6 game" 6 3;
-         make_play_random_game_test "8x8 game" 8 4;
+         make_play_random_game_test "8x8 game" 8 5;
        ]
 
 let _ =
   run_test_tt_main
     ("all tests"
     >::: [
-           make_connection_tests;
-           check_completed_box_tests;
-           completed_boxes_tests;
-           is_game_over_tests;
-           is_valid_move_test;
-           play_random_game_test;
+           check_completed_box_tests; is_valid_move_test; play_random_game_test;
          ])
